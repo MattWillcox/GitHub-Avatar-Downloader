61b270675985148ee2b1d924672df5a8e1249170
Matt Willcox <mattjwillcox@gmail.com>
2017-07-31 22:52:52 +0000

    Console Logs Contributor Avatar URLS

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,31 @@
 var request = require('request');
 
-var GITHUB_USER = "MattWillcox";
-var GITHUB_TOKEN = "a6ebeb0a66e22a53501a2c7fb5647fc7d34bef5d";
+const GITHUB_USER = "MattWillcox";
+const GITHUB_TOKEN = "a6ebeb0a66e22a53501a2c7fb5647fc7d34bef5d";
+const USER_AGENT = { 'User-Agent' : 'MattWillcox' };
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +
-   '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-   console.log(requestURL);
+  var requestURL = { url : 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +
+   '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+   headers :  USER_AGENT
+  }
+
+  request(requestURL, function(err, response, body) {
+    if (err) throw err;
+
+    console.log('Response Status Code:', response.statusCode);
+
+    cb(JSON.parse(body));
+
+  });
 }
 
-getRepoContributors('jquery', 'jquery', function(err){
-  console.log(err);
-});
+function findAvatar(data){
+  data.forEach(function(cont){
+    console.log(cont.avatar_url);
+  })
+}
+
+getRepoContributors('jquery', 'jquery', findAvatar);
