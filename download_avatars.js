de95402d48387be84b9432c1d4ef420cde35a04e
Matt Willcox <mattjwillcox@gmail.com>
2017-07-31 23:32:32 +0000

    Adds Avatar Downloader

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 const GITHUB_USER = "MattWillcox";
 const GITHUB_TOKEN = "a6ebeb0a66e22a53501a2c7fb5647fc7d34bef5d";
@@ -24,8 +25,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 function findAvatar(data){
   data.forEach(function(cont){
-    console.log(cont.avatar_url);
+      downloadImageByURL(cont.avatar_url, cont.login);
   })
+}
+
+function downloadImageByURL(url, filePath) {
+  request(url + '.jpeg', function(err, response, body) {
+  if (err) throw err;
+  //console.log('Avatar Response Status Code: ', response.statusCode);
+  }).pipe(fs.createWriteStream('./avatars/' + filePath + '.jpeg'));
 }
 
 getRepoContributors('jquery', 'jquery', findAvatar);
