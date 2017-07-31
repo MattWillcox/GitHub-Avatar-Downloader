212563a8809e569c92e8e3d95af181b88e979514
Matt Willcox <mattjwillcox@gmail.com>
2017-07-31 23:45:42 +0000

    Adds command line args and avatars folder to gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,17 @@
 var request = require('request');
 var fs = require('fs');
 
+var args = process.argv.slice(2);
+
 const GITHUB_USER = "MattWillcox";
 const GITHUB_TOKEN = "a6ebeb0a66e22a53501a2c7fb5647fc7d34bef5d";
 const USER_AGENT = { 'User-Agent' : 'MattWillcox' };
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+(!args[0] || !args[1]) ?
+  console.log("Error: Missing Command Line Arguments. \nPlease enter: node download_avatars.js <owner> <repo>")
+  : getRepoContributors(args[0], args[1], findAvatar);
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = { url : 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +
@@ -35,5 +41,3 @@ function downloadImageByURL(url, filePath) {
   //console.log('Avatar Response Status Code: ', response.statusCode);
   }).pipe(fs.createWriteStream('./avatars/' + filePath + '.jpeg'));
 }
-
-getRepoContributors('jquery', 'jquery', findAvatar);
