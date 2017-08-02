ba53e4ca3da34570f4420ad203440127b4662127
Matt Willcox <mattjwillcox@gmail.com>
2017-08-02 01:29:30 +0000

    Huge refactor by mentor

--- recommend.js
+++ recommend.js
@@ -4,8 +4,11 @@ require('dotenv').config();
 
 var args = process.argv.slice(2);
 
-const GITHUB_USER = process.env.USERNAME
-const GITHUB_TOKEN = process.env.TOKEN
+const GITHUB_USER = process.env.USERNAME;
+const GITHUB_TOKEN = process.env.TOKEN;
+
+let totalNumberOfContributors = 0;
+let contributorsSoFar = 0;
 
 if(!GITHUB_USER || !GITHUB_TOKEN){
   console.log("Error with .env file, please create .env file with the format:\nUSERNAME:<GitHub Username\nTOKEN:<GitHub Token>");
@@ -16,33 +19,110 @@ const USER_AGENT = { 'User-Agent': 'MattWillcox' };
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function countRepos(list) {
-  list.forEach(function(index){
-    request({ url: index,
-    headers: USER_AGENT
-    }, function(err, response, body) {
+function returnMax(list){
+  const modeMap = {};
+  let maxEl = [];
+  let maxCount = 1;
+  for(let k in list){
+    var el = list[k];
+
+    if (modeMap[el] == null)
+        modeMap[el] = 1;
+    else
+        modeMap[el]++;
+
+    if (modeMap[el] > maxCount)
+    {
+        maxEl = [el];
+        maxCount = modeMap[el];
+    }
+    else if (modeMap[el] == maxCount && maxEl[0] !== el)
+    {
+        maxEl.push(el);
+    }
+  }
+    console.log(maxEl);
+    return maxEl;
+}
+
+function countRepos(list, cb) {
+  let reposList = [];
+  let newList = [];
+  for (let i of list){
+    //console.log(`https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/users/${i}/starred`);
+    request({ url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/users/${i}/starred`,
+    headers: USER_AGENT }, function(err, response, body) {
       if (err) {
         throw err;
       }
-
-    console.log(JSON.parse(body));
-
-    })
-  };
+      for(let j of JSON.parse(body)){
+        reposList.push(j.name);
+      }
+      (cb(reposList));
+    });
+  }
 }
 
-function findRepos(data){
-  var urlArray = [];
-  data.forEach(function(cont){
-    urlArray.push(cont.starred_url.slice(0, -15));
+function sortRepoCounts(tracker) {
+  let rankedRepoNames = Object.keys(tracker).sort(function compareCounts(a, b) {
+    return tracker[b] - tracker[a];
   });
-  countRepos(urlArray);
+  if (contributorsSoFar == totalNumberOfContributors) {
+    console.log('Top 5 repos:');
+    rankedRepoNames.slice(0, 5).forEach(function (repoName) {
+      console.log('[' + tracker[repoName], "stars] -", repoName.replace('/', ' / '));
+    });
+  }
+}
 
+function getStarredRepos(user, collectReposCB, tracker) {
+  request({
+    url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/users/${user}/starred`,
+    headers: USER_AGENT
+  }, function (err, response, body) {
+    if (err) {
+      throw err;
+    }
+    collectReposCB(JSON.parse(body), tracker);
+    // console.log('repo counts:', JSON.stringify(tracker));
+    sortRepoCounts(tracker);
+  });
+}
+
+function processRepos(repoList, tracker) {
+  // console.log('body:', repoList);
+  var repoFullNames = repoList.map(function getFullName(repo) {
+    return repo.full_name;
+  });
+  repoFullNames.reduce(function (acc, repoName) {
+    acc[repoName] = acc[repoName] ? (acc[repoName] + 1) : 1;
+    return acc;
+  }, tracker);
+  contributorsSoFar++;
+  // repoFullNames.forEach(function addToTracker(repoName) {
+  //   tracker[repoName] = tracker[repoName] ? (tracker[repoName] + 1) : 1;
+  // });
+}
+
+function findStarredReposForContributors(data){
+  var contributorList = data.map(function (cont) {
+    return cont.login
+  });
+  console.log('list in findRepos:', contributorList);
+  var repoTracker = {};
+  totalNumberOfContributors = contributorList.length;
+  contributorList.forEach(function (contributor) {
+    getStarredRepos(contributor, processRepos, repoTracker);
+  });
+  // data.forEach(function(cont){
+
+  //   list.push(cont.login);
+  // });
+  // console.log(countRepos(contributorList, returnMax));
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = { url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN +
-  '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+  var requestURL = { url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
   headers: USER_AGENT
   };
 
@@ -62,4 +142,4 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 (!args[0] || !args[1] || args.length > 2) ?
   console.log("Error, Incorrect Command Line Arguments. \nPlease enter: node download_avatars.js <owner> <repo>")
-  : getRepoContributors(args[0], args[1], findRepos);
+  : getRepoContributors(args[0], args[1], findStarredReposForContributors);
